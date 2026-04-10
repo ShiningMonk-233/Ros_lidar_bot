78fc9a514972cad68718676e1653f69318642d13
ShiningMonk-233 <vimleshkumar3755@gmail.com>
2026-04-11 03:06:18 +0530

    IMU,Camera,Fixed Lidar, Map

--- launch/launch_sim.launch.py
+++ launch/launch_sim.launch.py
@@ -61,6 +61,8 @@ def generate_launch_description():
         get_package_share_directory(package_name), 'config', 'mapper_params_online_async.yaml'
     )
 
+    ekf_params_file = os.path.join(get_package_share_directory(package_name), 'config', 'ekf.yaml')
+
     slam_toolbox = IncludeLaunchDescription(
         PythonLaunchDescriptionSource([
             get_package_share_directory('slam_toolbox'),
@@ -71,26 +73,42 @@ def generate_launch_description():
             'use_sim_time': 'true'
         }.items()
     )
-
     bridge = Node(
         package='ros_gz_bridge',
         executable='parameter_bridge',
         arguments=[
-            '/odom@nav_msgs/msg/Odometry@ignition.msgs.Odometry',
-            '/cmd_vel@geometry_msgs/msg/Twist@ignition.msgs.Twist',
-            '/tf@tf2_msgs/msg/TFMessage@ignition.msgs.Pose_V',
-            '/scan@sensor_msgs/msg/LaserScan@ignition.msgs.LaserScan',
-            '/joint_states@sensor_msgs/msg/JointState@ignition.msgs.Model'
-
-
+            '/odom@nav_msgs/msg/Odometry[ignition.msgs.Odometry',
+            '/cmd_vel@geometry_msgs/msg/Twist]ignition.msgs.Twist',
+            '/scan@sensor_msgs/msg/LaserScan[ignition.msgs.LaserScan',
+            '/joint_states@sensor_msgs/msg/JointState[ignition.msgs.Model',
+            '/clock@rosgraph_msgs/msg/Clock[ignition.msgs.Clock',
+            # Add the IMU once we fix the URDF/World
+            '/imu@sensor_msgs/msg/Imu[ignition.msgs.IMU',
+            '/camera/image_raw@sensor_msgs/msg/Image[ignition.msgs.Image',
+            '/camera/camera_info@sensor_msgs/msg/CameraInfo[ignition.msgs.CameraInfo',
+        ],
+        remappings=[
+            ('/odom', '/odom_raw') # This sends Gazebo's /odom to EKF's input
         ],
         output='screen'
     )
+
+    # 2. EKF Node: Consumes /odom_raw + /imu -> Produces /odom
+    ekf_node = Node(
+        package='robot_localization',
+        executable='ekf_node',
+        name='ekf_filter_node',
+        output='screen',
+        parameters=[ekf_params_file, {'use_sim_time': True}],
+        remappings=[('/odometry/filtered', '/odom')] # Clean filtered data
+    )
+    
 
     return LaunchDescription([
         rsp,
         gz_sim,
         spawn_entity,
         bridge,
-        slam_toolbox
+        slam_toolbox,
+        ekf_node
     ])
